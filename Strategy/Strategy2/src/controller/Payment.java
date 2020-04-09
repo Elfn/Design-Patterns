1ed47a36f4169d9b7bb35a08fbbcdc0d04301026
Elimane Fofana <Elimane.Fofana@supinfo.com>
2020-04-09 23:49:48 +0000

    #2

---
+++ Strategy/Strategy2/src/controller/Payment.java
@@ -0,0 +1,12 @@
+package controller;
+
+/**
+ * Created by Elimane on Apr, 2020, at 07:13
+ */
+
+//Base interface to define strategy
+public interface Payment {
+
+    public void pay(int amount);
+
+}

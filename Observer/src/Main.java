4c51c203ef89ee2679a8bdace7e29cc26d338cc9
Elimane Fofana <Elimane.Fofana@supinfo.com>
2020-04-12 00:45:33 +0000

    Observer pattern #3

--- Observer/src/Main.java
+++ Observer/src/Main.java
@@ -5,6 +5,22 @@ import model.EmailTopicSubscriber;
 /**
  * Created by Elimane on Apr, 2020, at 22:50
  */
+
+/**
+* @Observer_Design
+ * Define one to many relationship between objects so that
+ * when an object's state change all its dependent objects
+ * are notified
+ *
+ * @PROS
+ * It supports the principle of loose coupling between objects that interact with each other.
+ * It allows sending data to other objects effectively without any change in the Subject or Observer classes.
+ *
+ * @CONS
+ * The Observer interface has to be implemented by ConcreteObserver , which involves inheritance. ...
+ * If not correctly implemented, the Observer can add complexity and lead to inadvertent performance issues.
+*/
+
 public class Main {
     public static void main(String[] args)
     {
@@ -33,6 +49,7 @@ public class Main {
 
         //Subject (Provider/Broadcaster)
         topic.postMessage("Hello subscribers!");
+        topic.postMessage("Hello  2!");
         //Will display :
 //            FirstObserver Retrieving message: Hello subscribers!
 //            SecondObserver Retrieving message: Hello subscribers!

74d642c6c46c5fc5b9c28c13dafe41a588cb89a6
Elimane Fofana <Elimane.Fofana@supinfo.com>
2020-05-02 16:32:52 +0000

    Department Notifier - OBSERVER DP #4

--- Observer/Observer-DepartmentNotifier/src/com/company/Main.java
+++ Observer/Observer-DepartmentNotifier/src/com/company/Main.java
@@ -16,20 +16,22 @@ public class Main {
         Subscriber subs2 = new Subscriber("John scott","js@gmail.com",true, Departments.HR);
         Subscriber subs3 = new Subscriber("Franck Doe","fd@gmail.com",true, Departments.CLIENT_SUPPORT);
         Subscriber subs4 = new Subscriber("Marie currie","mc@gmail.com",true, Departments.HR);
+        Subscriber subs5 = new Subscriber("Matt Tiger","mt@gmail.com",true, Departments.IT);
 
         //Register Observers to allows them receive message from provider
         provider.register(subs1);
         provider.register(subs2);
         provider.register(subs3);
         provider.register(subs4);
+        provider.register(subs5);
 
         //Send Message only to those in HR Department
-        provider.sendMessage("Employment","There are new resumes",Departments.HR);
+        provider.sendMessage("New Computers","There are new computers",Departments.IT);
         //Result :
 
-//        Message posted to department : HR
-//        John scott from HR department Retrieving message: Employment
-//        Marie currie from HR department Retrieving message: Employment
+//        Message posted to department : IT
+//        Eric Jean from IT department Retrieving message: New Computers
+//        Matt Tiger from IT department Retrieving message: New Computers
 
     }
 }

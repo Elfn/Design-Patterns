ae2ec9bb8295ba7de6f3845565f447367f415021
Elimane FOFANA <elimane.fofana@mediasoftci.net>
2021-07-27 16:50:51 +0000

    new

---
+++ Chain of responsibility/src/DirectorPurchasePower.java
@@ -0,0 +1,11 @@
+public class DirectorPurchasePower extends PurchasePower{
+    @Override
+    protected double getAllowable() {
+        return BASE * 100;
+    }
+
+    @Override
+    protected String getRole() {
+        return "DIRECTOR";
+    }
+}
